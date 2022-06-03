86946a6cf85774aa788628bc1c2e4ba4f51059f3
Rishad Amin Pulok <rishad15-2117@diu.edu.bd>
2022-06-03 20:28:05 +0600

    search option added

--- app/src/main/java/com/example/weatherapp/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/WeatherFragment.kt
@@ -1,12 +1,15 @@
 package com.example.weatherapp
 
 import android.content.ContentValues
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -22,6 +25,44 @@ class WeatherFragment : Fragment() {
 
     private val locViewModel: LocationViewModel by activityViewModels()
     private lateinit var binding: FragmentWeatherBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.weather_menu,menu)
+        val searchView = menu.findItem(R.id.item_search).actionView as SearchView
+        searchView.queryHint = "Search city by name"
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    convertQueryToLatLong(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                return true
+            }
+        })
+    }
+    private fun convertQueryToLatLong(query: String){
+        val geocoder = Geocoder(requireActivity())
+        val addressList: List<Address> = geocoder.getFromLocationName(query,1)
+        if(addressList.isNotEmpty()){
+            val lat = addressList[0].latitude
+            val lng = addressList[0].longitude
+            val location = Location("").apply {
+                latitude = lat
+                longitude = lng
+            }
+            locViewModel.setNewLocation(location)
+        }else{
+            Toast.makeText(requireActivity(), "Invalid city name!", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +81,7 @@ class WeatherFragment : Fragment() {
                 locViewModel.fetchData()
             }
             locViewModel.currentModelLiveData.observe(viewLifecycleOwner){
+                Log.d(ContentValues.TAG,"${it.main.temp}")
                 setCurrentData(it)
             }
             locViewModel.forecastModelLiveData.observe(viewLifecycleOwner){
@@ -56,11 +98,11 @@ class WeatherFragment : Fragment() {
         binding.showCityTV.text = "${it.name}, ${it.sys.country}"
         val iconUrl = "$icon_prefix${it.weather[0].icon}${icon_suffix}"
         Glide.with(requireActivity()).load(iconUrl).into(binding.showIconIV)
-        binding.showTempTV.text = it.main.temp.roundToInt().toString()
+        binding.showTempTV.text = "${it.main.temp.roundToInt().toString()}\u00B0"
         binding.showFeelTempTV.text = it.main.feels_like.roundToInt().toString()
         binding.showTempStatTV.text = it.weather[0].description
-        binding.showMaxTempTV.text = it.main.temp_max.roundToInt().toString()
-        binding.showMinTempTV.text = it.main.temp_min.roundToInt().toString()
+        binding.showMaxTempTV.text = "${it.main.temp_max.roundToInt().toString()}\u00B0"
+        binding.showMinTempTV.text = "${it.main.temp_min.roundToInt().toString()}\u00B0"
         binding.showSunriseTV.text = getFormattedDate(it.sys.sunrise, "HH:mm")
         binding.showSunsetTV.text = getFormattedDate(it.sys.sunset, "HH:mm")
 
